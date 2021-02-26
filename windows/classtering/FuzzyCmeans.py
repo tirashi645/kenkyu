18517cfe4d5625ff34353076d4547019831a59e9
tirashi645 <tirashi645@gmail.com>
2021-02-26 16:11:30 +0900

    c_means

---
+++ windows/classtering/FuzzyCmeans.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+def near(x, mu, m):
+    '''特徴ベクトルの各クラスタへの帰属度を返す'''
+    return [np.sum([(np.linalg.norm(x-mu[k]) / np.linalg.norm(x-mu[j])) ** (2/(m-1)) for j in range(mu.shape[0])]) ** -1 for k in range(mu.shape[0])]
+
+def clustering(X, K=3, m=1.5):
+    '''
+    X: 特徴ベクトルの集合
+    K: クラスタ数
+    m: Fuzzyパラメータ
+    '''
+    # データの総数
+    N = len(X)
+    # 前ステップの重心
+    old_mu = np.array([K, N])
+    # 寄与度
+    u = np.random.random([K, N])
+    e = np.inf
+    while True:
+        # 重心の算出
+        mu = np.dot(u, X) / np.array([np.sum(u, axis=1)]).T
+        # 寄与度の更新
+        for k in range(K):
+            for i in range(N):
+                u[k, i] = np.sum([(np.linalg.norm(X[i]-mu[k]) / np.linalg.norm(X[i]-mu[j]))**(2/(m-1)) for j in range(K)]) ** -1
+        _e = np.linalg.norm(mu-old_mu)
+        if _e > e:break
+        e = _e
+    print(mu)
+    return mu
+
+if __name__=='__main__':
+    # 学習するベクトル数
+    N = 100
+    # 特徴ベクトルの集合
+    X = np.random.random([N, 2])
+    # Fuzzyパラメータ
+    m = 2.0
+    # クラスタリング
+    mu = clustering(X, K=3, m=m)
+    # 各クラスタへの所属度を求める
+    near([0.1, 0.3], mu, m)
