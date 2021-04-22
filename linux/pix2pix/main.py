97bc882a49448726c721736453603ea79e49c56f
tirashi645 <tirashi645@gmail.com>
2021-04-22 11:21:13 +0900

    add

--- linux/pix2pix/main.py
+++ linux/pix2pix/main.py
@@ -24,7 +24,7 @@ log_dir = './tflog'
 datasetpath = '/media/koshiba/Data/pix2pix/output/datasetimages.hdf5'
 patch_size = 32
 batch_size = 12
-epoch = 1000
+epoch = 10
 
 def normalization(X):
     return X / 127.5 - 1
