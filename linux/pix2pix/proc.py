db7bd23d53e04b66e2aff6ddc6ff6929862ed277
tirashi645 <tirashi645@gmail.com>
2021-04-27 16:49:16 +0900

    add

--- linux/pix2pix/proc.py
+++ linux/pix2pix/proc.py
@@ -75,7 +75,8 @@ def plot_generated_batch(X_raw, generator_model, batch_size, b_id):
         plt.clf()
         plt.close()
         '''
-        cv2.imwrite(outputpath + "/proc/gen" + str(b_id) + '_' +str(i)+".jpg", np.array(X_gen[i]))
+        Xg = pil2cv(X_gen[i])
+        cv2.imwrite(outputpath + "/proc/gen" + str(b_id) + '_' +str(i)+".jpg", np.array(Xg)*255)
         cv2.imwrite(outputpath + "/proc/raw" + str(b_id) + '_' +str(i)+".jpg", np.array(X_raw[i]))
 
 def expand2square(pil_img, background_color):
