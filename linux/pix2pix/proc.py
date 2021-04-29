79f4fece3dc3456543b3e8c78430505ee0f27409
tirashi645 <tirashi645@gmail.com>
2021-04-29 19:16:30 +0900

    add

--- linux/pix2pix/proc.py
+++ linux/pix2pix/proc.py
@@ -155,7 +155,7 @@ def proc():
         gen_list = np.append(gen_list, proc_generator_batch(proc_batch, generator_model, batch_size, b_id, num, img_size))
         b_id += 1
     #gen_list = np.reshape([-1, height, width, 3])
-    print(org_list.shape)
+    print(gen_list.shape)
     for index in range(min(num, len(gen_list))):
         print(org_list[index].shape)
         cv2.imwrite(outputpath + "/proc_tmp/raw_" + name_list[index] +".jpg", org_list[index])
