27e9c1ae73d954b0c89ed9ed6a72d69e02757407
tirashi645 <tirashi645@gmail.com>
2021-05-07 17:00:26 +0900

    add

--- linux/pix2pix/makeData.py
+++ linux/pix2pix/makeData.py
@@ -6,13 +6,12 @@ import cv2
 from keras.preprocessing.image import load_img, img_to_array
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-'''
 inpath = '/media/koshiba/Data/pix2pix/input'
 outpath = '/media/koshiba/Data/pix2pix/output'
 '''
 inpath = './input'
 outpath = './output'
-
+'''
 
 # we create two instances with the same arguments
 data_gen_args1 = dict(featurewise_center=True,
