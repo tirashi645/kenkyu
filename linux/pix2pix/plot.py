5664b17ae428c804e9d0e37a1c4cb8c7d93386c5
tirashi645 <tirashi645@gmail.com>
2021-05-12 13:59:54 +0900

    add

--- linux/pix2pix/plot.py
+++ linux/pix2pix/plot.py
@@ -13,9 +13,17 @@ G_tot = [i[0] for i in G_loss]
 l1 = [i[1] for i in G_loss]
 G_logloss = [i[2] for i in G_loss]
 
-plt.plot(epochs, G_tot, color='blue')
-plt.plot(epochs, l1, color='green')
-plt.plot(epochs, G_logloss, color='orange')
-plt.plot(epochs, D_logloss, color='red')
+fig1 = plt.figure()
+fig2 = plt.figure()
+fig3 = plt.figure()
+fig4 = plt.figure()
 
-plt.savefig(outputpath + '/loss.jpg')
+fig1.plot(epochs, G_tot, color='blue')
+fig2.plot(epochs, l1, color='green')
+fig3.plot(epochs, G_logloss, color='orange')
+fig4.plot(epochs, D_logloss, color='red')
+
+fig1.savefig(outputpath + '/G_tot.jpg')
+fig2.savefig(outputpath + '/l1.jpg')
+fig3.savefig(outputpath + '/G_logloss.jpg')
+fig4.savefig(outputpath + '/D_logloss.jpg')
