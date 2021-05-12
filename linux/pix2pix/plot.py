a69c4a9e59da4e5d05e76eefea75787ff2a3123f
tirashi645 <tirashi645@gmail.com>
2021-05-12 10:52:15 +0900

    add

---
+++ linux/pix2pix/plot.py
@@ -0,0 +1,13 @@
+import pickle
+from matplotlib import pyplot as plt
+
+outputpath = '/media/koshiba/Data/pix2pix/output'
+
+with open(outputpath + '/gloss.pkl', 'rb') as f:
+    G_loss = pickle.load(f)
+with open(outputpath + '/dloss.pkl', 'rb') as f:
+    D_loss = pickle.load(f)
+
+epochs = len(G_loss)
+
+print(G_loss)
