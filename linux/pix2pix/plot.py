2b290554a9abd4aa81c662360a5ae54b08c0b271
tirashi645 <tirashi645@gmail.com>
2021-05-12 13:54:57 +0900

    add

--- linux/pix2pix/plot.py
+++ linux/pix2pix/plot.py
@@ -6,14 +6,16 @@ outputpath = '/media/koshiba/Data/pix2pix/output'
 with open(outputpath + '/gloss.pkl', 'rb') as f:
     G_loss = pickle.load(f)
 with open(outputpath + '/dloss.pkl', 'rb') as f:
-    D_loss = pickle.load(f)
+    D_logloss = pickle.load(f)
 
 epochs = [i+1 for i in range(len(D_loss))]
-print(G_loss[:][0])
+G_tot = [i[0] for i in G_loss]
+l1 = [i[1] for i in G_loss]
+G_logloss = [i[2] for i in G_loss]
 
-plt.plot(epochs, G_loss[:][0], color='blue')
-plt.plot(epochs, G_loss[:][1], color='green')
-plt.plot(epochs, G_loss[:][2], color='orange')
-plt.plot(epochs, D_loss, color='red')
+plt.plot(epochs, G_tot, color='blue')
+plt.plot(epochs, l1, color='green')
+plt.plot(epochs, G_logloss, color='orange')
+plt.plot(epochs, D_logloss, color='red')
 
 plt.savefig('loss')
