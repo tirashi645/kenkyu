1e4d0f5ba51e18b860361949b2557dc336d5d41e
tirashi645 <tirashi645@gmail.com>
2021-05-13 21:07:38 +0900

    add

--- windows/SeparatePict/test.py
+++ windows/SeparatePict/test.py
@@ -1,3 +1,11 @@
-name = 'D:/aaa/ddd/vvv.jpg'
+import os
+import shutil
+import glob
 
-print(name.split('/')[-1][:-4])
+data = glob.glob('D:/opticalflow/mask/**/*.png')
+for i in data:
+    i = i.replace(os.sep, '/')
+    dir_name = i.split('/')[-2]
+    img_name = i.split('/')[-1]
+    if not os.path.exists('D:/opticalflow/pict_all/pict/' + dir_name + '_' + img_name[:img_name.rfind('.')] + '.jpg'):
+        shutil.copy('D:/opticalflow/pict/' + dir_name + '/' + img_name[:img_name.rfind('.')] + '.jpg', 'D:/opticalflow/pict_all/pict/' + dir_name + '_' + img_name[:img_name.rfind('.')] + '.jpg')
