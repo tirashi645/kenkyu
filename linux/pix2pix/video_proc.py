c9c15994829155d15b18ed625d1b5f1d85eb607a
tirashi645 <tirashi645@gmail.com>
2021-10-15 01:18:29 +0900

    add

--- linux/pix2pix/video_proc.py
+++ linux/pix2pix/video_proc.py
@@ -18,7 +18,7 @@ def todo(path):
 
     # 読み込む動画の設定
     videoName = path.split('/')[-1][:-4]
-    savePath = '/media/koshiba/Data/pix2pix/output/proc_point_original_1000'
+    savePath = '/media/koshiba/Data/pix2pix/output/proc_point_original_100'
     cap = cv2.VideoCapture(path)
     print(path[path.rfind('/')+1:])
 
