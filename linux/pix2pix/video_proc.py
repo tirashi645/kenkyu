0f4f3139230028ed2ddc4397897959781249880e
tirashi645 <tirashi645@gmail.com>
2021-10-15 01:15:08 +0900

    add

--- linux/pix2pix/video_proc.py
+++ linux/pix2pix/video_proc.py
@@ -190,7 +190,7 @@ def todo(path):
     cv2.imwrite(savePath + '/' + videoName + '/org_' + videoName + '.jpg', org_img)
     cv2.imwrite(savePath + '/' + videoName + '/gray_' + videoName + '.jpg', first_gray)
     #cv2.imwrite(savePath + '/' + videoName + '/keypoint_' + videoName + '.jpg', keypoint_img)
-    cv2.imwrite('/media/koshiba/Data/pix2pix/output/proc_pict/' + videoName + '.jpg', frame3)
+    #cv2.imwrite('/media/koshiba/Data/pix2pix/output/proc_pict/' + videoName + '.jpg', frame3)
     with open(savePath + '/' + videoName + '/data_' + videoName + '.pickle', 'wb') as f:
         pickle.dump(evalute_list, f)
     return evalute_list
