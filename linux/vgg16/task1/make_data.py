e3236d30c95169a16ac9d5a9c98c4b996bd9cb4b
tirashi645 <tirashi645@gmail.com>
2021-11-02 16:53:39 +0900

    add

--- linux/vgg16/task1/make_data.py
+++ linux/vgg16/task1/make_data.py
@@ -1,8 +1,18 @@
 import glob
 import os
 
-cls_dirs = glob.glob('/media/koshiba/Data/sportConpetitive/train/**')
+li = ['train', 'test']
 
-for cls_dir in cls_dirs:
-    dir_name = cls_dir.split('/')[-1]
-    print(dir_name)
+for type_data in li:
+    cls_dirs = glob.glob('/media/koshiba/Data/sportConpetitive/'+type_data+'/**')
+
+    for cls_dir in cls_dirs:
+        dir_name = cls_dir.split('/')[-1]
+        print(dir_name)
+        files = glob.glob(cls_dir)
+        print('/media/koshiba/Data/sportConpetitive/vgg/'+type_data+'/'+dir_name)
+        '''
+        for i, file in enumerate(files):
+            if not os.path.exists('/media/koshiba/Data/sportConpetitive/vgg/'+type_data+'/'+dir_name):
+                os.makedirs('/media/koshiba/Data/sportConpetitive/vgg/'+type_data+'/'+dir_name)
+        '''
