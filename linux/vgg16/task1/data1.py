fadc59b43df3eb4544a79fb807ff3ad5f5619d31
tirashi645 <tirashi645@gmail.com>
2021-11-04 22:44:26 +0900

    add

--- linux/vgg16/task1/data1.py
+++ linux/vgg16/task1/data1.py
@@ -9,10 +9,11 @@ print(classPath)
 for i, cls in enumerate(classPath):
     dataPath = glob.glob(cls + '/**/*.jpg', recursive=True)
     cls_name = cls.split('/')[-1]
+    print(cls_name)
     
     for j, data in enumerate(dataPath):
-        if not os.path.exists('/media/koshiba/Data/sportConpetitive/vgg_16/train/' + cls_name):
-            os.makedirs('/media/koshiba/Data/sportConpetitive/vgg_16/train/' + cls_name)
+        if not os.path.exists('/media/koshiba/Data/sportConpetitive/vgg_data/train/' + cls_name):
+            os.makedirs('/media/koshiba/Data/sportConpetitive/vgg_data/train/' + cls_name)
         
-        shutil.copyfile(data, '/media/koshiba/Data/sportConpetitive/vgg_16/train/' + cls_name+'/'+cls_name+'_'+str(i)+'.jpg')
+        shutil.copyfile(data, '/media/koshiba/Data/sportConpetitive/vgg_data/train/' + cls_name+'/'+cls_name+'_'+str(i)+'.jpg')
         
--- linux/vgg16/task1/main.py
+++ linux/vgg16/task1/main.py
@@ -11,9 +11,9 @@ from tensorflow.keras.layers import Input, Flatten, Conv2D, MaxPooling2D, Dense,
 from tensorflow.keras.callbacks import Callback, EarlyStopping
 from tensorflow.keras.utils import to_categorical
 
-TRAIN_DIR = "/media/koshiba/Data/sportConpetitive/vgg/train/"
-TEST_DIR = "/media/koshiba/Data/sportConpetitive/vgg/test/"
-OUTPUT_DIR = "/media/koshiba/Data/sportConpetitive/vgg/output/"
+TRAIN_DIR = "/media/koshiba/Data/sportConpetitive/vgg16/train/"
+TEST_DIR = "/media/koshiba/Data/sportConpetitive/vgg16/test/"
+OUTPUT_DIR = "/media/koshiba/Data/sportConpetitive/vgg16/output/"
 
 ROWS = 150
 COLS = 150
