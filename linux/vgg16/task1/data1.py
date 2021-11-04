b0f5d40edfd724665d3324f5d75d13b3af2463f0
tirashi645 <tirashi645@gmail.com>
2021-11-04 23:06:58 +0900

    add

--- linux/vgg16/task1/data1.py
+++ linux/vgg16/task1/data1.py
@@ -4,7 +4,7 @@ import shutil
 
 li1 = ['train', 'test']
 
-classPath = glob.glob('/media/koshiba/Data/sportConpetitive/data2/**/')
+classPath = glob.glob('/media/koshiba/Data/sportConpetitive/data2/test/**/')
 print(classPath)
 for i, cls in enumerate(classPath):
     dataPath = glob.glob(cls + '/**/*.jpg', recursive=True)
@@ -12,10 +12,10 @@ for i, cls in enumerate(classPath):
     print(cls_name)
     
     for j, data in enumerate(dataPath):
-        if not os.path.exists('/media/koshiba/Data/sportConpetitive/vgg_data/train/' + cls_name):
-            os.makedirs('/media/koshiba/Data/sportConpetitive/vgg_data/train/' + cls_name)
+        if not os.path.exists('/media/koshiba/Data/sportConpetitive/vgg_data/test/' + cls_name):
+            os.makedirs('/media/koshiba/Data/sportConpetitive/vgg_data/test/' + cls_name)
         if not data.split('/')[-2]=='fig':
-            shutil.copyfile(data, '/media/koshiba/Data/sportConpetitive/vgg_data/train/'+cls_name+'/'+cls_name+'_'+str(j)+'.jpg')
+            shutil.copyfile(data, '/media/koshiba/Data/sportConpetitive/vgg_data/test/'+cls_name+'/'+cls_name+'_'+str(j)+'.jpg')
         else:
             print(data)
         
