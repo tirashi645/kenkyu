3a6987e40fbe81cfc0ee926bd59ecbd2d30c807f
tirashi645 <tirashi645@gmail.com>
2021-11-05 00:37:16 +0900

    add

--- linux/vgg16/task1/data1.py
+++ linux/vgg16/task1/data1.py
@@ -3,8 +3,10 @@ import glob
 import shutil
 
 li1 = ['train', 'test']
+cnt = 0
+tmp = ""
 
-classPath = glob.glob('/media/koshiba/Data/sportConpetitive/data2/test/**')
+classPath = glob.glob('/media/koshiba/Data/sportConpetitive/data2/**')
 print(classPath)
 for i, cls in enumerate(classPath):
     dataPath = glob.glob(cls + '/**/*.jpg', recursive=True)
@@ -12,10 +14,14 @@ for i, cls in enumerate(classPath):
     print(cls.split('/'))
     
     for j, data in enumerate(dataPath):
-        if not os.path.exists('/media/koshiba/Data/sportConpetitive/vgg_data/test/' + cls_name):
-            os.makedirs('/media/koshiba/Data/sportConpetitive/vgg_data/test/' + cls_name)
-        if not data.split('/')[-2]=='fig':
-            shutil.copyfile(data, '/media/koshiba/Data/sportConpetitive/vgg_data/test/'+cls_name+'/'+cls_name+'_'+str(j)+'.jpg')
-        else:
-            print(data)
+        if not os.path.exists('/media/koshiba/Data/sportConpetitive/vgg16/train/' + cls_name):
+            os.makedirs('/media/koshiba/Data/sportConpetitive/vgg16/train/' + cls_name)
+        if data.split('/')[-2]!=tmp:
+            cnt = 0
+        if cnt%30==0:
+            if not data.split('/')[-2]=='fig':
+                shutil.copyfile(data, '/media/koshiba/Data/sportConpetitive/vgg16/train/'+cls_name+'/'+cls_name+'_'+str(j)+'.jpg')
+            else:
+                print(data)
+        cnt += 1
         
