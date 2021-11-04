d4c2b32cf32c3964c3e345eb611bf969479cb5b5
tirashi645 <tirashi645@gmail.com>
2021-11-04 22:48:07 +0900

    add

--- linux/vgg16/task1/data1.py
+++ linux/vgg16/task1/data1.py
@@ -9,7 +9,7 @@ print(classPath)
 for i, cls in enumerate(classPath):
     dataPath = glob.glob(cls + '/**/*.jpg', recursive=True)
     cls_name = cls.split('/')[-2]
-    print(cls_name)
+    print(dataPath)
     
     for j, data in enumerate(dataPath):
         if not os.path.exists('/media/koshiba/Data/sportConpetitive/vgg_data/train/' + cls_name):
