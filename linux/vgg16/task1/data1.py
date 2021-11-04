053fa0b227ee96dad6126d88917d9c885932493f
tirashi645 <tirashi645@gmail.com>
2021-11-04 19:29:09 +0900

    add

--- linux/vgg16/task1/data1.py
+++ linux/vgg16/task1/data1.py
@@ -4,5 +4,5 @@ import glob
 classPath = glob.glob('/media/koshiba/Data/sportConpetitive/data2/')
 
 for cls in classPath:
-    dataPath = glob.glob(classPath + '/**/**/*')
+    dataPath = glob.glob(cls + '/**/**/*')
     print(dataPath[0])
