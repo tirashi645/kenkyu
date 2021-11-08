5412b5e7c6c09c14643c7f243f3460cd2082c6e3
tirashi645 <tirashi645@gmail.com>
2021-11-08 17:53:04 +0900

    add

--- linux/vgg16/task2/main.py
+++ linux/vgg16/task2/main.py
@@ -14,8 +14,8 @@ TRAIN_DIR = "/media/koshiba/Data/sportConpetitive/vgg16/train/"
 TEST_DIR = "/media/koshiba/Data/sportConpetitive/vgg16/test/"
 OUTPUT_DIR = "/media/koshiba/Data/sportConpetitive/vgg16/output/"
 
-ROWS = 150
-COLS = 150
+ROWS = 50
+COLS = 50
 CHANNELS = 3
 #print(os.listdir(TRAIN_DIR+'refree/'))
 
@@ -29,7 +29,7 @@ test_ow = [TEST_DIR+'ow/' + i for i in os.listdir(TEST_DIR+'ow/')]
 
 #test_images = [TEST_DIR + i for i in os.listdir(TEST_DIR)]
 train_images = train_refree + train_player + train_ow
-test_images = test_refree# + test_player + test_ow
+test_images = test_refree + test_player + test_ow
 
 random.shuffle(train_images)
 
