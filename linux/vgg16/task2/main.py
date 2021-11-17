be060563e50c966aee1db7f1b2f06991d7086646
tirashi645 <tirashi645@gmail.com>
2021-11-17 19:19:07 +0900

    add

--- linux/vgg16/task2/main.py
+++ linux/vgg16/task2/main.py
@@ -10,7 +10,7 @@ from tensorflow.keras.layers import Input, Flatten, Conv2D, MaxPooling2D, Dense,
 from tensorflow.keras.callbacks import Callback, EarlyStopping
 from tensorflow.keras.utils import to_categorical
 
-TRAIN_DIR = "/media/koshiba/Data/sportConpetitive/refree/train/"
+TRAIN_DIR = "/media/koshiba/Data/sportConpetitive/judo_data/refree/train/"
 TEST_DIR = "/media/koshiba/Data/sportConpetitive/refree/test/"
 OUTPUT_DIR = "/media/koshiba/Data/sportConpetitive/refree/output/"
 
