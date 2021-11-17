001f8f3106b13c318aef7a7c5556659afdd1175f
tirashi645 <tirashi645@gmail.com>
2021-11-18 05:29:28 +0900

    qee

--- linux/vgg16/task1/main.py
+++ linux/vgg16/task1/main.py
@@ -151,8 +151,8 @@ objective = 'categorical_crossentropy'
 # モデル構築
 def judo_model():
     input_tensor = Input(shape=(ROWS, COLS, CHANNELS))
-    vgg16 = ResNet50(include_top=False, weights='imagenet', input_tensor=input_tensor)
-    #vgg16 = VGG16(include_top=False, weights='imagenet', input_tensor=input_tensor)
+    #vgg16 = ResNet50(include_top=False, weights='imagenet', input_tensor=input_tensor)
+    vgg16 = VGG16(include_top=False, weights='imagenet', input_tensor=input_tensor)
     top_model = models.Sequential()
     top_model.add(Flatten(input_shape=vgg16.output_shape[1:]))
     #top_model.add(vgg16)
@@ -164,9 +164,9 @@ def judo_model():
     
     model = Model(inputs=vgg16.input, outputs=top_model(vgg16.output))
     
-    #for layer in top_model.layers[:15]:
-    #    layer.trainable = False
-    vgg16.trainable = False
+    for layer in top_model.layers[:15]:
+        layer.trainable = False
+    #vgg16.trainable = False
     
     '''
     top_model = Sequential()
@@ -208,8 +208,8 @@ early_stopping = EarlyStopping(monitor='val_loss', patience=10, verbose=1, mode=
 
 def run_judo_discriminator():
     history = LossHistory()
-    model.fit(train_data, train_labels, batch_size=batch_size, epochs=epochs, validation_split=0.3, verbose=1, shuffle=True, callbacks=[history])#, early_stopping])
-    #model.fit_generator(augmentation_train_data, steps_per_epoch=20 , epochs=120, validation_data=augmentation_validation_data, validation_steps=30, callbacks=[history, early_stopping])
+    #model.fit(train_data, train_labels, batch_size=batch_size, epochs=epochs, validation_split=0.3, verbose=1, shuffle=True, callbacks=[history])#, early_stopping])
+    model.fit_generator(augmentation_train_data, steps_per_epoch=20 , epochs=120, validation_data=augmentation_validation_data, validation_steps=30, callbacks=[history, early_stopping])
     
     predictions = model.predict(test_data, verbose=1)
     return predictions, history
