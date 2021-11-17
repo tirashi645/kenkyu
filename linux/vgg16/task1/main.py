769f2a65a03ca7c498252879b6465a6c59e1a6c6
tirashi645 <tirashi645@gmail.com>
2021-11-17 21:47:52 +0900

    add

--- linux/vgg16/task1/main.py
+++ linux/vgg16/task1/main.py
@@ -43,6 +43,7 @@ validation_images = validation_refree + validation_player + validation_ow#[::10]
 test_images = test_refree + test_player + test_ow#[::10]
 
 random.shuffle(train_images)
+random.shuffle(validation_images)
 
 # 画像をリサイズして統一
 def read_image(file_path):
@@ -200,7 +201,7 @@ early_stopping = EarlyStopping(monitor='val_loss', patience=10, verbose=1, mode=
 
 def run_judo_discriminator():
     history = LossHistory()
-    model.fit(train_data, train_labels, batch_size=batch_size, epochs=epochs, validation_split=0.3, verbose=1, shuffle=True, callbacks=[history])#, early_stopping])
+    model.fit(train_data, train_labels, batch_size=batch_size, epochs=epochs, validation_split=0.3, verbose=1, shuffle=True, callbacks=[history, early_stopping])
     #model.fit_generator(augmentation_train_data, steps_per_epoch=30 , epochs=30, validation_data=augmentation_validation_data, validation_steps=30, callbacks=[history, early_stopping])
     
     predictions = model.predict(test_data, verbose=1)
