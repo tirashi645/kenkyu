c353da37b541a3927d3f7bfd4ffddf11a5a5363d
tirashi645 <tirashi645@gmail.com>
2021-11-18 01:18:40 +0900

    add

--- linux/vgg16/task1/main.py
+++ linux/vgg16/task1/main.py
@@ -154,7 +154,7 @@ def judo_model():
     top_model.add(Dense(512, activation='relu', kernel_initializer='he_normal'))
     top_model.add(Dropout(0.5))
     #top_model.add(Dense(60, activation='relu', kernel_initializer='he_normal'))
-    top_model.add(Dense(2, activation='sigmoid'))
+    top_model.add(Dense(3, activation='sigmoid'))
     
     model = Model(inputs=vgg16.input, outputs=top_model(vgg16.output))
     
