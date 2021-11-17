fd35a515c98641aa5f514d9d57e383502ebf0807
tirashi645 <tirashi645@gmail.com>
2021-11-17 20:01:38 +0900

    add

--- linux/vgg16/task1/main.py
+++ linux/vgg16/task1/main.py
@@ -113,7 +113,8 @@ augmentation_train_datagen = ImageDataGenerator(
     rescale = 1./255
     )
 #学習用のバッチの生成
-augmentation_train_data = augmentation_train_datagen.flow(train_data, train_labels, batch_size=32, seed=0)
+augmentation_train_data = augmentation_train_datagen.flow(train_data, train_labels, batch_size=32, seed=0)\
+augmentation_validation_data = augmentation_train_datagen.flow(train_data, train_labels, batch_size=32, seed=0)
 
 # 最適化アルゴリズム
 optimizer = 'SGD'
@@ -167,7 +168,7 @@ early_stopping = EarlyStopping(monitor='val_loss', patience=10, verbose=1, mode=
 def run_judo_discriminator():
     history = LossHistory()
     #model.fit(train_data, train_labels, batch_size=batch_size, epochs=epochs, validation_split=0.3, verbose=1, shuffle=True, callbacks=[history, early_stopping])
-    model.fit_generator(augmentation_train_data, batch_size=batch_size, epochs=epochs, validation_split=0.3, verbose=1, shuffle=True, callbacks=[history, early_stopping])
+    model.fit_generator(augmentation_train_data, steps_per_epoch=30 , epochs=20, validation_split=0.3, verbose=1, shuffle=True, callbacks=[history, early_stopping])
     
     predictions = model.predict(test_data, verbose=1)
     return predictions, history
