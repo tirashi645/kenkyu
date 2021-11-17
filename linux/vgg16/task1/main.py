51b4490e4dbb3505929e5937592c8a95de2b6e8e
tirashi645 <tirashi645@gmail.com>
2021-11-18 01:37:51 +0900

    add

--- linux/vgg16/task1/main.py
+++ linux/vgg16/task1/main.py
@@ -187,7 +187,7 @@ model = judo_model()
 # number of epochs
 epochs = 120
 # batch_size
-batch_size = 15
+batch_size = 32
 
 # monitor the trend of losses
 class LossHistory(Callback):
@@ -204,8 +204,8 @@ early_stopping = EarlyStopping(monitor='val_loss', patience=10, verbose=1, mode=
 
 def run_judo_discriminator():
     history = LossHistory()
-    model.fit(train_data, train_labels, batch_size=batch_size, epochs=epochs, validation_split=0.3, verbose=1, shuffle=True, callbacks=[history])#, early_stopping])
-    #model.fit_generator(augmentation_train_data, steps_per_epoch=20 , epochs=120, validation_data=augmentation_validation_data, validation_steps=30, callbacks=[history, early_stopping])
+    #model.fit(train_data, train_labels, batch_size=batch_size, epochs=epochs, validation_split=0.3, verbose=1, shuffle=True, callbacks=[history])#, early_stopping])
+    model.fit_generator(augmentation_train_data, steps_per_epoch=20 , epochs=120, validation_data=augmentation_validation_data, validation_steps=30, callbacks=[history, early_stopping])
     
     predictions = model.predict(test_data, verbose=1)
     return predictions, history
