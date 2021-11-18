ad93f2e520860520883f4e98ff90398af92c34f0
tirashi645 <tirashi645@gmail.com>
2021-11-18 10:48:58 +0900

    add

--- linux/vgg16/task1/data1.py
+++ linux/vgg16/task1/data1.py
@@ -8,4 +8,4 @@ for i, file in enumerate(filePath):
     print(file)
     im = cv2.imread(file)
     im = cv2.resize(im, (150, 150), interpolation=cv2.INTER_CUBIC)
-    cv2.imwrite('/media/koshiba/Data/sport/**/**/*.jpg', im)
+    cv2.imwrite(file, im)
