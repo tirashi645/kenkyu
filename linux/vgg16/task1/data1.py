9c69e31ed5daa05537644c91201ac46b019df3c4
tirashi645 <tirashi645@gmail.com>
2021-11-18 10:54:59 +0900

    add

--- linux/vgg16/task1/data1.py
+++ linux/vgg16/task1/data1.py
@@ -9,7 +9,7 @@ for i, file in enumerate(filePath):
     img = cv2.imread(file)
     height,width = img.shape[:2]
 
-    target_size = (150,150) #src size < dst sizeの前提
+    target_size = (height,width) #src size < dst sizeの前提
 
     top = int((target_size[1] - height)/2)
     bottom = target_size[1] - height - top
@@ -18,4 +18,5 @@ for i, file in enumerate(filePath):
 
     color = (255,255,255)
     img = cv2.copyMakeBorder(img, top, bottom, left, right,cv2.BORDER_CONSTANT,value=color)
+    img = cv2.resize(img, (150, 150), interpolation=cv2.INTER_CUBIC)
     cv2.imwrite(file, img)
