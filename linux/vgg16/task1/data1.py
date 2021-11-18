e5e0c14f1da0aceacd7a50171303e8ce783026bc
tirashi645 <tirashi645@gmail.com>
2021-11-18 10:53:29 +0900

    add

--- linux/vgg16/task1/data1.py
+++ linux/vgg16/task1/data1.py
@@ -6,6 +6,16 @@ import cv2
 filePath = glob.glob('/media/koshiba/Data/sport/**/**/*.jpg')
 for i, file in enumerate(filePath):
     print(file)
-    im = cv2.imread(file)
-    im = cv2.resize(im, (150, 150), interpolation=cv2.INTER_CUBIC)
-    cv2.imwrite(file, im)
+    img = cv2.imread(file)
+    height,width = img.shape[:2]
+
+    target_size = (150,150) #src size < dst sizeの前提
+
+    top = int((target_size[1] - height)/2)
+    bottom = target_size[1] - height - top
+    left = int((target_size[0] - width)/2)
+    right = target_size[0] - width - left
+
+    color = (255,255,255)
+    img = cv2.copyMakeBorder(img, top, bottom, left, right,cv2.BORDER_CONSTANT,value=color)
+    cv2.imwrite(file, img)
