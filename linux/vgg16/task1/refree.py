ef4a61825d42f2f08173cdd2957e6a8897d0e378
tirashi645 <tirashi645@gmail.com>
2021-12-02 18:34:58 +0900

    add

--- linux/vgg16/task1/refree.py
+++ linux/vgg16/task1/refree.py
@@ -100,14 +100,10 @@ augmentation_train_datagen = ImageDataGenerator(
     rotation_range = 10,
     #左右反転
     horizontal_flip = True,
-    #上下平行移動
-    height_shift_range = 0.2,
     #左右平行移動
     width_shift_range = 0.2,
     #ランダムにズーム
     zoom_range = 0.2,
-    #チャンネルシフト
-    channel_shift_range = 0.2,
     #スケーリング
     rescale = 1./255
     )
@@ -177,8 +173,8 @@ early_stopping = EarlyStopping(monitor='val_loss', patience=10, verbose=1, mode=
 
 def run_judo_discriminator():
     history = LossHistory()
-    model.fit(train_data, train_labels, batch_size=batch_size, epochs=epochs, validation_split=0.3, verbose=1, shuffle=True, callbacks=[history])#, early_stopping])
-    #model.fit_generator(augmentation_train_data, steps_per_epoch=30 , epochs=100, validation_data=augmentation_validation_data, validation_steps=30, callbacks=[history, early_stopping])
+    #model.fit(train_data, train_labels, batch_size=batch_size, epochs=epochs, validation_split=0.3, verbose=1, shuffle=True, callbacks=[history])#, early_stopping])
+    model.fit_generator(augmentation_train_data, steps_per_epoch=30 , epochs=100, validation_data=augmentation_validation_data, validation_steps=30, callbacks=[history, early_stopping])
     
     predictions = model.predict(test_data, verbose=1)
     return predictions, history
