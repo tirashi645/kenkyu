4ca9de9d88e3517fd6a1dbdf435e9aea0829f988
tirashi645 <tirashi645@gmail.com>
2021-12-09 08:02:53 +0900

    add

--- linux/vgg16/task1/refree.py
+++ linux/vgg16/task1/refree.py
@@ -39,7 +39,7 @@ train_tmp = [TRAIN_DIR+'tmp/' + i for i in os.listdir(TRAIN_DIR+'tmp/')]
 
 
 #test_images = [TEST_DIR + i for i in os.listdir(TEST_DIR)]
-train_images = train_refree + train_player + train_ow + train_tmp
+train_images = train_refree + train_player + train_ow# + train_tmp
 #test_images = test_refree + test_player + test_ow
 
 random.shuffle(train_images)
