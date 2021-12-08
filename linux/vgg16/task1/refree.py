c1686ca0372aa48230550954676db8d955281815
tirashi645 <tirashi645@gmail.com>
2021-12-08 20:26:26 +0900

    add

--- linux/vgg16/task1/refree.py
+++ linux/vgg16/task1/refree.py
@@ -144,10 +144,10 @@ def judo_model():
     #vgg16.trainable = False
     '''
     model = Sequential()
-    model.add(Conv2D(6, kernel_size=(5,5), activation='relu', kernel_initializer='he_normal', input_shape=(ROWS, COLS, CHANNELS)))
-    model.add(MaxPooling2D(pool_size=(2,2)))
-    model.add(Conv2D(16, kernel_size=(5,5), activation='relu', kernel_initializer='he_normal'))
-    model.add(MaxPooling2D(pool_size=(2,2)))
+    #model.add(Conv2D(6, kernel_size=(5,5), activation='relu', kernel_initializer='he_normal', input_shape=(ROWS, COLS, CHANNELS)))
+    #model.add(MaxPooling2D(pool_size=(2,2)))
+    #model.add(Conv2D(16, kernel_size=(5,5), activation='relu', kernel_initializer='he_normal'))
+    #model.add(MaxPooling2D(pool_size=(2,2)))
     model.add(Flatten())
     model.add(Dense(120, activation='relu', kernel_initializer='he_normal'))
     model.add(Dense(60, activation='relu', kernel_initializer='he_normal'))
