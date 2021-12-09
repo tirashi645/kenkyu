c6d432f98f67353931f8d065a43a3ba551871795
tirashi645 <tirashi645@gmail.com>
2021-12-09 13:42:35 +0900

    add

--- linux/vgg16/task1/main5.py
+++ linux/vgg16/task1/main5.py
@@ -111,8 +111,8 @@ train_labels = to_categorical(train_labels, 2)
 #validation_labels = to_categorical(validation_labels, 3)
 test_labels = to_categorical(test_labels, 2)
 
-model = load_model(OUTPUT_DIR + 'refree_model4.h5')
-model.load_weights(OUTPUT_DIR + 'refree_model4_weight.h5')
+model = load_model(OUTPUT_DIR + 'refree_model5.h5')
+model.load_weights(OUTPUT_DIR + 'refree_model5_weight.h5')
 
 x_test = prep_data(test_images)
 print(model.predict(x_test))
