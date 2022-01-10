df725573c0cda50318923db6c2f6ddb77a90c409
tirashi645 <tirashi645@gmail.com>
2022-01-10 10:03:51 +0900

    add

--- linux/vgg16/task1/refree2.py
+++ linux/vgg16/task1/refree2.py
@@ -20,7 +20,8 @@ from sklearn.model_selection import train_test_split
 TEST_DIR = "/media/koshiba/Data/sportConpetitive/judo_data/refree2/test/"
 OUTPUT_DIR = "/media/koshiba/Data/sportConpetitive/refree/output/"
 
-TRAIN_DIR = "/media/koshiba/Data/sportConpetitive/judo_data/refree_skeleton/"
+#TRAIN_DIR = "/media/koshiba/Data/sportConpetitive/judo_data/refree_skeleton/"
+TRAIN_DIR = "/media/koshiba/Data/sportConpetitive/judo_data/hantei/"
 
 ROWS = 150
 COLS = 150
